ea08e70fd277eaba6ab11b83c292d837a0fb7c89
IceDog23 <dkaplos32@gmail.com>
2021-03-22 12:25:11 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+
+//Copy your url here
+const publicSpreadSheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTAP8kAxe06y9YkCkGWAwPsXWLUWnXdhv8Yd3SITp_cpyqjidkkWVD3_vkl0HH3lO1t_whHhr1xAEJt/pub?gid=0&single=true&output=csv'
+
+function init() {
+    Papa.parse(publicSpreadSheetUrl , {
+    download: true,
+    header: false,
+    complete: info
+  })
+}
+
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+
+function info (resutl) { 
+    const data = resutl.data
+
+    for (let elem of data) {
+        //Creating needed elements
+        const art = document.createElement('article')
+        const div = document.createElement('div')
+        div.className = 'title'
+        const spanName = document.createElement('span')
+        spanName.innerText = elem[0]
+        const spanPrice = document.createElement('span')    
+        spanPrice.innerText = elem[2]
+        spanPrice.className = 'price'    
+
+        //Placing elements into HTML
+        const root = document.querySelector('#root')
+        root.append(art)
+        art.append(div)
+        div.append(spanName)
+        div.append(spanPrice)
+    }
+}
+
+
+
+
+
+
+
